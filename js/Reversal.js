8f1ffb344879adc2108ebc2af97e593a6898b9b4
Pippin Barr <pippin.barr@gmail.com>
2020-03-10 10:36:25 -0400

    Reversal: got the basic version of it working
    ? It is sufficiently weird that I think there's some interest to it as a 
    variation. I don't really understand how to play well. One instant 
    observation is that a standard knight move instantly loses the game 
    because it puts you in check right away which is funny.
    
    ? It's satisfying of course to just get something working while in the 
    background I'm struggling to understand how to do v r 4. I think making 
    some more chesses is a solid way to just get my brain back in order.

---
+++ js/Reversal.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+class Reversal extends BaseChess {
+
+  constructor() {
+    super();
+
+    // CHECKMATE POSITION
+    // this.game.load("2rnkbnr/4pppp/4pbpp/7q/8/3QPPPP/3RPPPP/2NBKBNR w - - 0 7");
+    // this.board.position(this.game.fen(),false);
+
+    // STALEMATE POSITION
+    // this.game.load("5Rnk/7n/7R/8/8/8/7R/6QK w - - 0 7");
+    // this.board.position(this.game.fen(),false);
+  }
+
+  move(from, to) {
+    let move = super.move(from, to);
+    this.board.position(this.game.fen(), true);
+    setTimeout(() => {
+      this.game.put({
+        type: move.piece,
+        color: move.color === 'b' ? 'w' : 'b'
+      }, to);
+      this.board.position(this.game.fen(), false);
+    }, this.config.moveSpeed);
+  }
+
+}
