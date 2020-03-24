827e41ae829259862600cd33528fd3d8e452417f
Pippin Barr <pippin.barr@gmail.com>
2020-03-24 19:45:38 -0400

    Tidying: MUSICAL
    ? Also deleted 1D and LIFE properly

--- js/Musical.js
+++ js/Musical.js
@@ -1,117 +1,122 @@
 "use strict";
 
+// Musical Chess
+//
+// Treats the board as a tone matrix played left to right.
+// Ranks represent specific notes from a chord repeated up octaves.
+// Black and white play separately when it's their turn, with only
+// their pieces counting as notes in the tone matrix.
+// White plays an ACE chord
+// Black plays an FAC chord
 
 class Musical extends BaseChess {
 
   constructor() {
     super();
 
-    // EMPTY POSITION
-    // this.game.load("8/8/8/8/8/8/8/8 w - - 0 7");
-    // this.board.position(this.game.fen(), false);
-
-    // CHECKMATE POSITION
-    // this.game.load("2rnkbnr/4pppp/4pbpp/7q/8/3QPPPP/3RPPPP/2NBKBNR w - - 0 7");
-    // this.board.position(this.game.fen(),false);
-
-    // STALEMATE POSITION
-    // this.game.load("5Rnk/7n/7R/8/8/8/7R/6QK w - - 0 7");
-    // this.board.position(this.game.fen(),false);
-
+    // How often to play a note (by file)
     this.MUSIC_INTERVAL = 1000;
+    // The file to play next (file as in chess)
+    this.file = 0;
+
+    // We have one synth per color per rank, so we use arrays
     this.whiteSynths = [];
     this.blackSynths = [];
-    let whiteFrequencies = [
+    // The frequencies for either side are represented in Hz for each
+    // rank, with 0 being the player's back rank and so on.
+    const whiteFrequencies = [
       // Amaj ACE chord
       220, 261.63, 329.63, 440, 523.25, 659.25, 880, 1046.50
     ];
-    let blackFrequencies = [
-      //Amag FAC chord
+    const blackFrequencies = [
+      //Amaj FAC chord
       174.61, 220, 261.63, 349.23, 440, 523.25, 698.46, 880, 1046.50
     ];
 
+    // Loop through the frequencies and create synths set to each frequency
+    // (We need multiple synths to play multiple notes at the same time)
     for (let i = 0; i < 8; i++) {
-      this.whiteSynths.push(new Pizzicato.Sound({
-        source: 'wave',
-        options: {
-          frequency: whiteFrequencies[i],
-          attack: 0.4,
-          release: 0.4
-        }
-      }));
-
-      this.blackSynths.push(new Pizzicato.Sound({
-        source: 'wave',
-        options: {
-          frequency: blackFrequencies[i],
-          attack: 0.4,
-          release: 0.4
-        }
-      }));
+      // Add synths with the appropriate frequency
+      this.whiteSynths.push(this.createSynth(whiteFrequencies[i]));
+      this.blackSynths.push(this.createSynth(blackFrequencies[i]));
     }
-    this.file = 0;
 
+    // Add a div to all squares that can display whether that square is being played
     $(SQUARE).append('<div class="note-playing"></div>');
+
+    // Create the turn indicator by adding a border to the top and bottom
+    // ranks that we can switch the color of between black and yellow
     for (let i = 0; i < 8; i++) {
-      $(`.square-${FILES[i]}1`).addClass('white-file-not-playing');
-      $(`.square-${FILES[i]}8`).addClass('black-file-not-playing');
+      $(`.square-${FILES[i]}1`).addClass('w-file-not-playing');
+      $(`.square-${FILES[i]}8`).addClass('b-file-not-playing');
     }
     $(`.square-a1`).addClass('white-file-playing');
 
+    // Start playing our music as an interval
     this.playInterval = setInterval(() => {
       this.play();
     }, this.MUSIC_INTERVAL);
   }
 
-  changeTurn() {
-    if (this.gameOver) return;
-    if (this.game.turn() === 'w') {
-      setTimeout(() => {
-        // $('.board-b72b1').removeClass('blackTurn', 250);
-        // $('.board-b72b1').addClass('whiteTurn', 250, () => {
-        this.enableInput();
-        this.from = null;
-      }, 500);
-    }
-    else {
-      // $('.board-b72b1').removeClass('whiteTurn', 250);
-      // $('.board-b72b1').addClass('blackTurn', 250, () => {
-      setTimeout(() => {
-        this.enableInput();
-        this.from = null;
-      }, 500);
-    }
+  // Creates a new synth object at the specified frequency
+  createSynth(frequency) {
+    return new Pizzicato.Sound({
+      source: 'wave',
+      options: {
+        frequency: frequency,
+        attack: 0.4,
+        release: 0.4
+      }
+    })
   }
 
+  // Plays the notes for the current file and turn
   play() {
-    let $squares = $(`div[class*="square-${FILES[this.file]}"]`);
-    let context = this;
+    let turn = this.game.turn();
+    // Get the current file
     let file = FILES[this.file];
-    $(SQUARE).removeClass('white-file-playing');
-    $(SQUARE).removeClass('black-file-playing');
-    if (this.game.turn() === 'w') {
-      $(`.square-${file}1`).addClass('white-file-playing');
-    }
-    else {
-      $(`.square-${file}8`).addClass('black-file-playing');
-    }
-    $squares.each(function() {
-      let square = $(this).data('square');
-      let piece = context.game.get(square);
-      if (piece && context.game.turn() === piece.color) {
+    // Get all squares in the current file using a resgular expression on the CSS
+    let $squares = $(`div[class*="square-${file}"]`);
+    // Remove any indicators
+    $(SQUARE).removeClass('w-file-playing');
+    $(SQUARE).removeClass('b-file-playing');
+    // Add the appropriate turn indicator
+    $(`.square-${file}${turn === this.game.WHITE ? 1 : 8}`)
+      .addClass(`${turn}-file-playing`);
+
+    // Go through every rank for the current file...
+    for (let rank = 1; rank <= 8; rank++) {
+      let square = `${file}${rank}`;
+      // Get the piece on the square (if any)
+      let piece = this.game.get(square);
+      // If there's a piece of the current turn's color, we need to play it
+      if (piece && turn === piece.color) {
+        // Add the indicator to the square
         $(`.square-${square} .note-playing`).show();
-        let rank = RANKS.indexOf(square[1]);
-        let synth = piece.color === 'w' ? context.whiteSynths[rank] : context.blackSynths[7 - rank];
+        // Get the correct synth based on color
+        let synth = piece.color === this.game.WHITE ? this.whiteSynths[rank] : this.blackSynths[8 - rank];
+        // Play it
         synth.play();
+        // Stop the synth after an interval (and remove the indicator)
         setTimeout(() => {
           synth.stop();
           setTimeout(() => {
             $('.note-playing').hide();
-          }, context.MUSIC_INTERVAL / 2 - synth.release * 1000);
-        }, context.MUSIC_INTERVAL / 2 - synth.release * 1000);
+          }, this.MUSIC_INTERVAL / 2 - synth.release * 1000);
+        }, this.MUSIC_INTERVAL / 2 - synth.release * 1000);
       }
-    });
+    };
+    // Having played all the notes for this step, move to the next file for the next one
     this.file = (this.file + 1) % FILES.length;
+  }
+
+  // Override changeTurn() so we only display our file indicator as the turn indicator
+  changeTurn() {
+    if (this.gameOver) return;
+    setTimeout(() => {
+      this.enableInput();
+      this.from = null;
+    }, 250);
   }
 
   // When quitting this game we need to stop the music from playing
